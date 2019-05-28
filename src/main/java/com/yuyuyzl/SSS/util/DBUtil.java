e27ee49f170d5b292a945801692ca2ec825fba57
for0nething <245575413@qq.com>
2019-05-28 22:13:26 +0800

    将数据库改成了服务器上的

--- src/main/java/com/yuyuyzl/SSS/util/DBUtil.java
+++ src/main/java/com/yuyuyzl/SSS/util/DBUtil.java
@@ -5,9 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    public static final String URL="jdbc:mysql://localhost:3306/rg_trial?useSSL=false";;
+    //public static final String URL="jdbc:mysql://localhost:3306/rg_trial?useSSL=false";;
+    //public static final String USER="root";
+    //public static final String PASSWORD="wangjiayi";
+
+    public static final String URL="jdbc:mysql://132.232.169.70/sss-team?useSSL=false" + "?autoReconnect=true&useUnicode=true&useSSL=false"
+            + "&characterEncoding=utf-8&serverTimezone=UTC";
+    ;
     public static final String USER="root";
-    public static final String PASSWORD="wangjiayi";
+    public static final String PASSWORD="8280508";
     private static Connection conn=null;
     static{
         try{
