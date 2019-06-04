969d7aef1df581853b592611ecd21f2a3f152839
zlccccc <940182901@qq.com>
2019-06-05 02:37:36 +0800

    test ES service

--- src/main/java/com/sss/provider/dao/ESdao.java
+++ src/main/java/com/sss/provider/dao/ESdao.java
@@ -62,21 +62,21 @@ public class ESdao {
         return matchResult;
     }
 
-    public static List<Map<String, Object>> searchMultiple(String type, List<Pair<String,List<Pair<String,Pair<String,String>>>>> limits,
+    public static List<Map<String, Object>> searchMultiple(String type, List<Pair<String, List<Pair<String, Pair<String, String>>>>> limits,
                                                            int start, int count) throws IOException {
         SearchRequest searchRequest = new SearchRequest(indexName);
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
         BoolQueryBuilder andBoolQueryBuilder = new BoolQueryBuilder();
-        for (Pair<String,List<Pair<String,Pair<String,String>>>> limit:limits){
+        for (Pair<String, List<Pair<String, Pair<String, String>>>> limit : limits) {
             BoolQueryBuilder orBoolQueryBuilder = new BoolQueryBuilder();
             String name = limit.getKey();
-            for (Pair<String,Pair<String,String>> orVal:limit.getValue()) {
-                String value=orVal.getKey(),way=orVal.getValue().getKey(),fuzz=orVal.getValue().getValue();
+            for (Pair<String, Pair<String, String>> orVal : limit.getValue()) {
+                String value = orVal.getKey(), way = orVal.getValue().getKey(), fuzz = orVal.getValue().getValue();
                 QueryBuilder QueryBuilder;
                 if (way.equals("matchQuery")) {
-                    QueryBuilder = new MatchQueryBuilder(name,value);
+                    QueryBuilder = new MatchQueryBuilder(name, value);
                 } else if (way.equals("fuzzyQuery")) {
-                    QueryBuilder = new FuzzyQueryBuilder(name,value);
+                    QueryBuilder = new FuzzyQueryBuilder(name, value);
                 } else if (way.equals("rangeQuery")) {
                     if (value.equals("gt"))
                         QueryBuilder = new RangeQueryBuilder(name).gt(Integer.parseInt(fuzz));
@@ -91,9 +91,9 @@ public class ESdao {
                         continue;
                     }
                 } else if (way.equals("termQuery")) {
-                    QueryBuilder=new TermQueryBuilder(name,value);
-                } else if (way.equals("wildcardQuery")){
-                    QueryBuilder=new WildcardQueryBuilder(name,value);
+                    QueryBuilder = new TermQueryBuilder(name, value);
+                } else if (way.equals("wildcardQuery")) {
+                    QueryBuilder = new WildcardQueryBuilder(name, value);
                 } else {
                     System.out.println("no! Query way读取失败");
                     continue;
@@ -120,6 +120,32 @@ public class ESdao {
         return matchResult;
     }
 
+    public static List<Map<String, Object>> searchTable(String type, List<Pair<String, Integer>> limits,
+                                                        String keyword, int start, int count) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(indexName);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder andBoolQueryBuilder = new BoolQueryBuilder();
+        for (Pair<String, Integer> now : limits) {
+            FuzzyQueryBuilder fuzzyQueryBuilder = new FuzzyQueryBuilder(now.getKey(), keyword);
+            andBoolQueryBuilder.should(fuzzyQueryBuilder.boost(now.getValue()));
+        }
+        sourceBuilder.query(andBoolQueryBuilder);
+        //确定要开始搜索的结果索引
+        sourceBuilder.from(start);
+        //返回的搜索匹配数
+        sourceBuilder.size(count);
+        searchRequest.source(sourceBuilder);
+        searchRequest.types(type);
+        //匹配度从高到低
+        sourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        SearchResponse searchResponse = client.search(searchRequest);
+        SearchHits hits = searchResponse.getHits();
+        List<Map<String, Object>> matchResult = new LinkedList<Map<String, Object>>();
+        for (SearchHit hit : hits.getHits()) {
+            matchResult.add(hit.getSourceAsMap());
+        }
+        return matchResult;
+    }
 
     // 获得指定type指定id的数据 json
     public static Map getDocument(String type, String id) throws IOException {
@@ -138,7 +164,7 @@ public class ESdao {
     }
 
     public static boolean checkExistIndex(String indexName) throws IOException {
-        boolean result =true;
+        boolean result = true;
         try {
             OpenIndexRequest openIndexRequest = new OpenIndexRequest(indexName);
             client.indices().open(openIndexRequest).isAcknowledged();
@@ -148,17 +174,17 @@ public class ESdao {
                 result = false;
             }
         }
-        if(result)
-            System.out.println("索引:" +indexName + " 是存在的");
+        if (result)
+            System.out.println("索引:" + indexName + " 是存在的");
         else
-            System.out.println("索引:" +indexName + " 不存在");
+            System.out.println("索引:" + indexName + " 不存在");
         return result;
     }
 
     public static void deleteIndex(String indexName) throws IOException {
         DeleteIndexRequest request = new DeleteIndexRequest(indexName);
         client.indices().delete(request);
-        System.out.println("删除了索引："+indexName);
+        System.out.println("删除了索引：" + indexName);
     }
 
     @Override
@@ -170,17 +196,21 @@ public class ESdao {
         // TODO Auto-generated method stub
         CreateIndexRequest request = new CreateIndexRequest(indexName);
         client.indices().create(request);
-        System.out.println("创建了索引："+indexName);
+        System.out.println("创建了索引：" + indexName);
     }
+
     public static String getIndexName() {
         return indexName;
     }
+
     public static void setIndexName(String indexName) {
         ESdao.indexName = indexName;
     }
+
     public static RestHighLevelClient getClient() {
         return client;
     }
+
     public static void setClient(RestHighLevelClient client) {
         ESdao.client = client;
     }
--- src/main/java/com/sss/provider/service/ESService.java
+++ src/main/java/com/sss/provider/service/ESService.java
@@ -33,7 +33,6 @@ public class ESService implements IESService {
     }
 
     public List<Map<String, Object>> Query(String name, List<Pair<String, List<Pair<String, Pair<String, String>>>>> limits, int count) {
-        if (count < 100) count = 100;
         try {
             assert (indexNames.contains(name));
             name = name.toLowerCase() + "res";
@@ -45,19 +44,44 @@ public class ESService implements IESService {
         }
     }
 
+    public List<Map<String, Object>> FuzzyQueryString(String name, String keyword, int count) {
+        try {
+            assert (indexNames.contains(name));
+            List<Pair<String,Integer>> L  = new LinkedList<Pair<String, Integer>>();
+            if (name.equals("expert")){
+                L.add(new Pair("name",10));
+                L.add(new Pair("orgs",9));
+                L.add(new Pair("publid",4));
+                L.add(new Pair("tags",4));
+            } else if (name.equals("paper")||name.equals("patent")) {
+                L.add(new Pair("title",10));
+                L.add(new Pair("abstract",6));
+            } else {
+                System.out.println("no! 表名错误");
+                return null;
+            }
+            name = name.toLowerCase() + "res";
+            ESdao.setIndexName(name);
+            return ESdao.searchTable("query", L,keyword,0, count);
+        } catch (Exception e) {
+            System.out.println("IO Exception happens\n  " + e.getMessage());
+            return null;
+        }
+    }
+
     // TODO: TEST-OK ; no use
     // 在这里删索引啥的
     public static void main(String[] args) {
         // TODO: 修改查询代码, 现在的查询可能不对
         ESService testService = new ESService();
-        /*
         // 更改数据库
-        try{
-            ESdao.deleteIndex("indexres");
-        }catch (Exception e){
-            System.out.println(e);
-        }
-        */
+//        try{
+////            ESdao.deleteIndex("expertres");
+//            ESdao.deleteIndex("paperres");
+//            ESdao.deleteIndex("patentres");
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
         /*
         // testFuzzyQuery
         List<Map<String, Object>> res = testService.FuzzyQuery("expert", "position", "posion", 0);
@@ -71,17 +95,21 @@ public class ESService implements IESService {
         }
         System.out.println("end");
         */
-        List<Pair<String, List<Pair<String, Pair<String, String>>>>> limits = new LinkedList<Pair<String, List<Pair<String, Pair<String, String>>>>>();
-        LinkedList<Pair<String, Pair<String, String>>> fir=new LinkedList<Pair<String, Pair<String, String>>>();
+//        List<Pair<String, List<Pair<String, Pair<String, String>>>>> limits = new LinkedList<Pair<String, List<Pair<String, Pair<String, String>>>>>();
+//        LinkedList<Pair<String, Pair<String, String>>> fir=new LinkedList<Pair<String, Pair<String, String>>>();
 //        fir.add(new Pair("search",new Pair("rangeQuery","sss")));
 //        limits.add(new Pair("abstract",fir));
-        fir.add(new Pair("lt",new Pair("rangeQuery","2005")));
-        limits.add(new Pair("year",fir));
-        List<Map<String, Object>> res = testService.Query("paper",limits, 100);
-        if (res == null) {
-            System.out.println("数据获取失败");
-            return;
-        }
+////        fir.add(new Pair("lt",new Pair("rangeQuery","2005")));
+////        limits.add(new Pair("year",fir));
+//        List<Map<String, Object>> res = testService.Query("paper",limits, 100);
+//        if (res == null) {
+//            System.out.println("数据获取失败");
+//            return;
+//        }
+//        for (Map<String, Object> now : res) {
+//            System.out.println(now.toString());
+//        }
+        List<Map<String, Object>> res=testService.FuzzyQueryString("expert", "sss", 100);
         for (Map<String, Object> now : res) {
             System.out.println(now.toString());
         }
